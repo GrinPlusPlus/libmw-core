72d596d7fda13bf00dbb00a7ee682c4bdac75edd
David Burkett <davidburkett38@gmail.com>
2019-12-27 12:52:31 -0500

    Common library

---
+++ src/common/LoggerImpl.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include <mw/common/Logger.h>
+
+class Logger
+{
+public:
+    static Logger& GetInstance();
+
+    void StartLogger(
+        const std::string& logDirectory,
+        const spdlog::level::level_enum& logLevel
+    );
+    void Log(const LoggerAPI::LogFile file, const spdlog::level::level_enum logLevel, const std::string& eventText);
+    void Flush();
+
+private:
+    Logger() = default;
+
+    std::shared_ptr<spdlog::logger> GetLogger(const LoggerAPI::LogFile file);
+
+    std::shared_ptr<spdlog::logger> m_pNodeLogger;
+    std::shared_ptr<spdlog::logger> m_pWalletLogger;
+};
